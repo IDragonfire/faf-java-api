e694ca16adadc8b5a7f1fb59babc9fc8f56aa87a
Dragonfire <bastian616-git@yahoo.de>
2017-01-08 03:08:14 +0100

    Cleanup Clan model

--- src/main/java/com/faforever/api/data/domain/Clan.java
+++ src/main/java/com/faforever/api/data/domain/Clan.java
@@ -1,18 +1,20 @@
 package com.faforever.api.data.domain;
 
+import com.yahoo.elide.annotation.CreatePermission;
 import com.yahoo.elide.annotation.Include;
 import com.yahoo.elide.annotation.UpdatePermission;
+import lombok.Data;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "clan_list")
 @Include(rootLevel = true, type = "clan")
 @UpdatePermission(expression = "is leader")
+@Data
 public class Clan {
 
   private int clanId;
@@ -28,53 +30,31 @@ public class Clan {
   private List<Player> members;
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "clan_id")
   public int getClanId() {
     return clanId;
   }
 
-  public void setClanId(int clanId) {
-    this.clanId = clanId;
-  }
-
-  @Basic
   @Column(name = "create_date")
   public Timestamp getCreateDate() {
     return createDate;
   }
 
-  public void setCreateDate(Timestamp createDate) {
-    this.createDate = createDate;
-  }
-
-  @Basic
   @Column(name = "status")
   public int getStatus() {
     return status;
   }
 
-  public void setStatus(int status) {
-    this.status = status;
-  }
-
-  @Basic
   @Column(name = "clan_name")
   public String getClanName() {
     return clanName;
   }
 
-  public void setClanName(String clanName) {
-    this.clanName = clanName;
-  }
 
-  @Basic
   @Column(name = "clan_tag")
   public String getClanTag() {
     return clanTag;
-  }
-
-  public void setClanTag(String clanTag) {
-    this.clanTag = clanTag;
   }
 
   @ManyToOne
@@ -83,69 +63,28 @@ public class Clan {
     return clanFounder;
   }
 
-  public void setClanFounder(Player clanFounderId) {
-    this.clanFounder = clanFounderId;
-  }
-
   @ManyToOne
   @JoinColumn(name = "clan_leader_id")
   public Player getClanLeader() {
     return clanLeader;
   }
 
-  public void setClanLeader(Player clanLeaderId) {
-    this.clanLeader = clanLeaderId;
-  }
-
-  @Basic
   @Column(name = "clan_desc")
   public String getClanDesc() {
     return clanDesc;
   }
 
-  public void setClanDesc(String clanDesc) {
-    this.clanDesc = clanDesc;
-  }
-
-  @Basic
   @Column(name = "clan_tag_color")
   public String getClanTagColor() {
     return clanTagColor;
   }
 
-  public void setClanTagColor(String clanTagColor) {
-    this.clanTagColor = clanTagColor;
-  }
-
   @ManyToMany
   @JoinTable(
-          name="clan_membership",
-          joinColumns=@JoinColumn(name="clan_id", referencedColumnName="clan_id"),
-          inverseJoinColumns=@JoinColumn(name="player_id", referencedColumnName="id"))
-  public List<Player> getMembers() { return this.members; }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(clanId, createDate, status, clanName, clanTag, clanFounder, clanLeader, clanDesc, clanTagColor);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Clan clan = (Clan) o;
-    return clanId == clan.clanId &&
-        status == clan.status &&
-        clanFounder == clan.clanFounder &&
-        clanLeader == clan.clanLeader &&
-        Objects.equals(createDate, clan.createDate) &&
-        Objects.equals(clanName, clan.clanName) &&
-        Objects.equals(clanTag, clan.clanTag) &&
-        Objects.equals(clanDesc, clan.clanDesc) &&
-        Objects.equals(clanTagColor, clan.clanTagColor);
+          name = "clan_membership",
+          joinColumns = @JoinColumn(name = "clan_id", referencedColumnName = "clan_id"),
+          inverseJoinColumns = @JoinColumn(name = "player_id", referencedColumnName = "id"))
+  public List<Player> getMembers() {
+    return this.members;
   }
 }
