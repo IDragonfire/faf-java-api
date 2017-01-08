bf1db17dc5712478c2273d5aa027444dc4ce6613
Dragonfire <bastian616-git@yahoo.de>
2017-01-08 14:52:48 +0100

    Restrict all data manipulation operation

--- src/main/java/com/faforever/api/data/package-info.java
+++ src/main/java/com/faforever/api/data/package-info.java
@@ -1,4 +1,12 @@
 /**
  * Contains classes to access data according to the <a href="http://jsonapi.org/">JSON-API specification</a>.
  */
+// By default restrict all data manipulation operation
+@UpdatePermission(any={Role.NONE.class })
+@CreatePermission(any={Role.NONE.class })
+@DeletePermission(any={Role.NONE.class })
+@SharePermission(any={Role.NONE.class })
 package com.faforever.api.data;
+
+import com.yahoo.elide.annotation.*;
+import com.yahoo.elide.security.checks.prefab.Role;
